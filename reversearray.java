0abba32096dacf42aa4be462fd92334a6d9fe76d
Kiran <93365995+Kirankumar37@users.noreply.github.com>
2025-02-05 19:29:11 +0530

    Create reversearray.java

---
+++ reversearray.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+
+            obj.reverseArray(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        reverse(0,arr.length-1,arr);
+    }
+    public void reverse(int start, int end, int arr[]){
+        if(start>end){
+            return;
+        }
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        reverse(start+1,end-1,arr);
+    }
+}
